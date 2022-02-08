9801819dbdf366a03abd697bfc2a966078f4c8b7
Sakie Kawamichi <sakie0303@gmail.com>
2022-02-08 06:49:20 +0000

    [Finish]ルーティング完了

---
+++ config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+  root :to => "homes#top"
+  #get "top" => "homes#top"
+  get "books" => "books#index", as: "book"
+  post "books" => "books#create"
+  get "books/:id" => "books#show", as: "show"
+  get "books/:id/edit" => "books#edit", as: "edit_book"
+  patch "books/:id" => "books#update", as: "update_book"
+  delete "books/:id" => "books#destroy", as: "destroy_book"
+
+
+   #post "books" => "books#create"
+  # get "books" => "books#index"
+   #get "edit" => "books#edit"
+  #patch "books/:id"
+
+
+
+
+
+
+
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
