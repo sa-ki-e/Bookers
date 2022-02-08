df6eb0d0fcfbecb1fe65572f314f055c7496d2cf
Sakie Kawamichi <sakie0303@gmail.com>
2022-02-08 12:27:23 +0000

    [Add]失敗した時のバリデーション追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,13 +9,18 @@ class BooksController < ApplicationController
   end
   
   def create
-   book = Book.new(book_params)
-   book.save
-   redirect_to "/index"
+   @book = Book.new(book_params)
+   if @book.save
+   flash[:notice] = "Book was successfully created."
+    redirect_to show_path(@book.id)
+   else
+    @books = Book.all
+    render :index
+   end
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.find(4398)
   end
 
   def edit
@@ -23,11 +28,21 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to update_book_path(book.id)
+    else
+    render :edit
+   end
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to book_path
+  end
   
   private
   def book_params
