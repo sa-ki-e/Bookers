2b012e60503d0f0e815f47a3a4477a9595f40ca1
Sakie Kawamichi <sakie0303@gmail.com>
2022-02-10 04:28:12 +0000

    [Finish]Bookers：再提出

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,18 @@
 class BooksController < ApplicationController
-  
-  
+
+
 
   def index
     @books = Book.all
     @book = Book.new
-    
+
   end
-  
+
   def create
    @book = Book.new(book_params)
    if @book.save
    flash[:notice] = "Book was successfully created."
-    redirect_to show_path(@book.id)
+    redirect_to book_path(@book.id)
    else
     @books = Book.all
     render :index
@@ -20,30 +20,30 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(4398)
+    @book = Book.find(params[:id])
   end
 
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
     flash[:notice] = "Book was successfully updated."
-    redirect_to update_book_path(book.id)
+    redirect_to book_path(@book.id)
     else
     render :edit
-   end
+    end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     flash[:notice] = "Book was successfully destroyed."
-    redirect_to book_path
+    redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root :to => "homes#top"
   #get "top" => "homes#top"
-  get "books" => "books#index", as: "book"
-  post "books" => "books#create"
-  get "books/:id" => "books#show", as: "show"
-  get "books/:id/edit" => "books#edit", as: "edit_book"
-  patch "books/:id" => "books#update", as: "update_book"
-  delete "books/:id" => "books#destroy", as: "destroy_book"
+  # get "books" => "books#index", as: "book"
+  # post "books" => "books#create"
+  # get "books/:id" => "books#show", as: "show"
+  # get "books/:id/edit" => "books#edit", as: "edit_book"
+  # patch "books/:id" => "books#update", as: "update_book"
+  # delete "books/:id" => "books#destroy", as: "destroy_book"
 
-
+  resources :books
    #post "books" => "books#create"
   # get "books" => "books#index"
    #get "edit" => "books#edit"
